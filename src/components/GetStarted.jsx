0f6a91fb3e1dd4d57679917afe98c21e54dc4f00
Matthew McGovern <mattmcg37@gmail.com>
2024-02-29 11:19:13 -0300

    hero pt 1

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import styles from '../style';
+import { arrowUp } from '../assets';
+
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className=>
+
+      </div>
+    </div>
+
+
   )
 }
 
