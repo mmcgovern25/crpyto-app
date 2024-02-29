827ba428e75258edbbfb7f618674f784539318b9
Matthew McGovern <mattmcg37@gmail.com>
2024-02-29 12:36:36 -0300

    stats done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Business = () => {
   return (
-    <div>B</div>
+    <div>Business</div>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
           annual fees.</p>
       </div>
 
-      <div>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img
           src={robot}
           alt='billing'
@@ -49,6 +49,10 @@ const Hero = () => {
         <div className='absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient'/>
         <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
       </div>
+
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
+    </div>
 
 
     </section>
