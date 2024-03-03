2d3152c8cd3cc28236312aa13d15aa3bfab62121
Matthew McGovern <mattmcg37@gmail.com>
2024-03-03 17:24:29 -0300

    clients

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,7 +2,7 @@ import { quotes } from '../assets';
 
 const FeedbackCard = ({ content, name, title, img }) => {
   return (
-    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:m4-5 mr-0 my-5 feedback-card'>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[335px] md:mr-10 sm:m4-5 mr-0 my-5 feedback-card'>
       <img
         src={quotes}
         alt='double_quotes' className='w-[42px] h-[27px] object-contain'
@@ -14,7 +14,7 @@ const FeedbackCard = ({ content, name, title, img }) => {
 
           <div className='flex flex-col ml-4'>
             <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white'>{name}</h4>
-            <h4 className='font-poppins font-semibold text-[16px] leading-[24px] text-dimWhite'>{title}</h4>
+            <h4 className='font-poppins font-semibold text-[16px] leading-[24px] text-white'>{title}</h4>
           </div>
         </div>
     </div>
