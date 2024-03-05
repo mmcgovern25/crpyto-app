fc5b3c3ab3198f735eb1fa24d11669aa8d0eb8de
Matthew McGovern <mattmcg37@gmail.com>
2024-03-05 16:52:21 -0300

    get started makeover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import '../Button.css';
+import React from "react";
 
-const Button = ({ styles, children }) => {
-  return (
-    <button type='button'  className={`button ${styles} button-theme`}>
-    {children}
-    <span></span><span></span><span></span><span></span>
-    Get Started
-    </button>
-  )
-}
+const Button = ({ styles }) => (
+  <button
+    type="button"
+    className={`relative h-12 w-40 overflow-hidden button-gradient font-semibold text-black shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-yellow-500 hover:before:-translate-x-40`}
+  >
+    <span className="relative z-10 font-semibold">Get Started</span>
+  </button>
+);
 
-export default Button
+
+
+export default Button;
