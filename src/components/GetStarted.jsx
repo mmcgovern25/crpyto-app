81fb6de020024441deb2d417302fed07bf51cac6
Matthew McGovern <mattmcg37@gmail.com>
2024-03-05 17:12:28 -0300

    new getstarted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,21 +1,27 @@
-import CoolButton from "../CoolButton.css"
+import styles from '../style';
+import { arrowUp } from '../assets';
 
 
 const GetStarted = () => {
   return (
-    <button className="my-super-cool-btn">
-      <span>
-        <div className="dots-container">
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer button-hover getstarted-theme`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] mr-'>
+            <span className='text-gradient'>Get</span>
+          </p>
+          <img
+              src={arrowUp}
+              alt='arrow'
+              className='w-[23px] h-[23px] object-contain' />
         </div>
-        Get Started
-      </span>
-    </button>
 
+        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+            <span className='text-gradient'>Started</span>
+          </p>
 
+      </div>
+    </div>
   )
 }
 
