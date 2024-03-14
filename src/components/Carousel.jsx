5663fe363bee3c20ca640f08643eecc605feb2fb
Matthew McGovern <mattmcg37@gmail.com>
2024-03-14 18:18:58 -0300

    headache. lot harder than i thought

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,48 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { TrendingCoins } from '../config/api';
-import "react-alice-carousel/lib/alice-carousel.css";
-import AliceCarousel from 'react-alice-carousel';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../config/api";
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
 
   const fetchTrendingCoins = async () => {
-    try {
-      const { data } = await axios.get(TrendingCoins('USD'));
-      setTrending(data);
-    } catch (error) {
-      console.error("Error fetching trending coins:", error);
-      console.error("Error response data:", error.response.data); // Log response data if available
-      console.error("Error response status:", error.response.status); // Log response status if available
-      console.error("Error message:", error.message); // Log error message
-    }
+    const { data } = await axios.get(TrendingCoins());
+    setTrending(data);
   };
-  console.log(trending)
 
   useEffect(() => {
     fetchTrendingCoins();
-  }, []); // Empty dependency array to run the effect only once on component mount
-
+  }, []);
 
   const items = trending.map((coin) => {
-      return (
-        <Link to={`/coins/${coin.id}`} className='flex flex-col items-center cursor-pointer uppercase text-white h-20' key={''}>
+    let profit = coin?.price_change_percentage_24h >= 0;
+
+    return (
+      <div className="flex flex-row items-center justify-center cursor-pointer uppercase text-white" key={coin.id}>
+        <Link
+          to={`/coins/${coin.id}`}
+          className="flex items-center"
+        >
           <img
             src={coin?.image}
             alt={coin.name}
+            height="80"
             style={{ marginBottom: 10 }}
-
           />
-
-          <span>{coin?.symbol}
+          <span>
+            {coin?.symbol}
             &nbsp;
+            <span
+              className={
+                profit ? "text-green-500 font-semibold" : "text-red-500 font-semibold"
+              }
+            >
+              {profit && "+"}
+              {coin?.price_change_percentage_24h?.toFixed(2)}%
+            </span>
           </span>
-
+          <span className="text-lg font-semibold">
+            {numberWithCommas(coin?.current_price.toFixed(2))}
+          </span>
         </Link>
-      )
-  })
+      </div>
+    );
+  });
 
   const responsive = {
     0: {
@@ -53,19 +64,18 @@ const Carousel = () => {
     },
   };
 
-
   return (
-    <div className="h-1/2 flex items-center">
+    <div className="flex flex-row items-center h-1/2">
       <AliceCarousel
         mouseTracking
         infinite
         autoPlayInterval={1000}
-        animationDuratiion={1500}
+        animationDuration={1500}
         disableDotsControls
         disableButtonsControls
-            responsive={responsive}
-            autoPlay
-            items={items}
+        responsive={responsive}
+        items={items}
+        autoPlay
       />
     </div>
   );
